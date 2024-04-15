57208f365361aee51eb80e1648795cf84812aea5
Valeriy Petrenko <fourcher00@gmail.com>
2024-04-15 14:24:34 -0400

    Sidebard Created, minor Avatar style changes

--- components/users/ActiveUsers.tsx
+++ components/users/ActiveUsers.tsx
@@ -10,7 +10,7 @@ const ActiveUsers = () => {
   const hasMoreUsers = users.length > 3;
   const memoizedUsers = useMemo(() => {
     return (
-      <div className="flex items-center justify-center gap-1">
+      <div className="flex items-center justify-center gap-1 py-2">
       <div className="flex pl-3">
         {currentUser && (
           <Avatar name="You" otherStyles="border-3[px] border-primary-green" />
